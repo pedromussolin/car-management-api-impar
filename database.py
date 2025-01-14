977f1852de987b7c5969c0e0eebf0ad140a5eae1
Pedro Henrique Mussolin Ayres Netto <104608112+pedromussolin@users.noreply.github.com>
2025-01-14 00:29:25 -0300

    Limpeza database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://provaadmin:!C^pc]Bp68$NF>-@avaliacaobackend.postgres.database.azure.com:5432/postgres'
+URL_DATABASE = 'postgresql://usuario:senha@host:porta/postgres'
 
 engine = create_engine(URL_DATABASE)
 
